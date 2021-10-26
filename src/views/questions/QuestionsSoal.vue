fe1de5e4ff3474e4b79e608ca7e6bcef6aa6a5a4
rommzz <rommy_zohara@gmail.com>
2021-10-26 08:10:54 +0700

    base list pertanyaan

--- src/views/questions/QuestionsSoal.vue
+++ src/views/questions/QuestionsSoal.vue
@@ -21,103 +21,152 @@
           class="ma-0"
           @click="newDialog = true"
         >
-          tambah soal
+          edit data soal
         </v-btn>
       </v-card-title>
-      <template v-if="data.soal.length">
+      <template v-if="data.soal">
         <v-list
           subheader
           two-line
         >
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title>Judul</v-list-item-title>
-              <v-list-item-subtitle>Set the content filtering level to restrict appts that can be downloaded</v-list-item-subtitle>
+              <v-list-item-title class="text-h4">
+                Judul
+              </v-list-item-title>
+              <v-list-item-subtitle class="text-body-1">
+                {{ data.judul }}
+              </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title>Matpel</v-list-item-title>
-              <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
+              <v-list-item-title class="text-h4">
+                Matpel
+              </v-list-item-title>
+              <v-list-item-subtitle class="text-body-1">
+                {{ data.matpel }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h4">
+                Jenis
+              </v-list-item-title>
+              <v-list-item-subtitle class="text-body-1">
+                {{ data.jenis }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h4">
+                Kelas
+              </v-list-item-title>
+              <v-list-item-subtitle class="text-body-1">
+                {{ data.kelas }}
+              </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-list>
-        <v-card-text>
-          <div>
-            <span>Judul: </span><span class="text-capitalize">{{ data.judul }}</span>
-          </div>
-          <div>
-            <span>Matpel: </span><span class="text-capitalize">{{ data.matpel }}</span>
-          </div>
-        </v-card-text>
-        <v-simple-table>
-          <thead>
-            <tr>
-              <th class="primary--text">
-                Judul Soal
-              </th>
-              <th class="primary--text">
-                Mata pelajaran
-              </th>
-              <th class="primary--text">
-                Kelas
-              </th>
-              <th class="primary--text">
-                Jenis
-              </th>
-              <th class="text-right primary--text">
-                Aksi
-              </th>
-            </tr>
-          </thead>
+        <v-sheet
+          class="container"
+          elevation="5"
+        >
+          <v-row>
+            <v-col>
+              <h3 class="pl-1">
+                Soal
+              </h3>
+            </v-col>
+            <v-spacer />
+            <v-col class="text-right">
+              <v-btn
+                color="primary"
+                @click="addSoal()"
+              >
+                tambah soal
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-simple-table>
+            <thead>
+              <tr>
+                <th class="primary--text">
+                  No
+                </th>
+                <th
+                  colspan="3"
+                  class="primary--text"
+                >
+                  Soal
+                </th>
+                <th class="text-right primary--text">
+                  Aksi
+                </th>
+              </tr>
+            </thead>
 
-          <tbody>
-            <tr
-              v-for="(item, index) in data"
-              :key="index"
-            >
-              <td>{{ item.judul }}</td>
-              <td>{{ item.matpel }}</td>
-              <td>{{ item.kelas }}</td>
-              <td>{{ item.jenis }}</td>
-              <td class="text-right">
-                <v-tooltip top>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      small
-                      color="secondary"
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      <v-icon>
-                        mdi-pencil
-                      </v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Edit Soal</span>
-                </v-tooltip>
+            <tbody>
+              <tr
+                v-for="(item, index) in data.soal"
+                :key="index"
+              >
+                <td>
+                  {{ index+1 }}
+                </td>
+                <td colspan="3">
+                  <div style="text-transform: capitalize">
+                    {{ item.soal }}
+                  </div>
+                  <div v-if="item.jenis == 'pilihan_ganda'">
+                    <div>
+                      <span class="primary--text">a.</span> jembot kreweol
+                    </div>
+                  </div>
+                </td>
+                <td class="text-right">
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        small
+                        color="secondary"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <v-icon>
+                          mdi-pencil
+                        </v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Edit Soal</span>
+                  </v-tooltip>
 
-                <v-tooltip top>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      small
-                      class="ml-1"
-                      color="red"
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      <v-icon>
-                        mdi-delete
-                      </v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Hapus Soal</span>
-                </v-tooltip>
-              </td>
-            </tr>
-          </tbody>
-        </v-simple-table>
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        small
+                        class="ml-1"
+                        color="red"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <v-icon>
+                          mdi-delete
+                        </v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Hapus Soal</span>
+                  </v-tooltip>
+                </td>
+              </tr>
+            </tbody>
+          </v-simple-table>
+        </v-sheet>
       </template>
       <v-card-text v-else>
         belum ada pertanyaan
@@ -181,6 +230,7 @@
 </template>
 <script>
   import axios from 'axios'
+  const abjad = ('abcdefghijklmnopqrstuvwxyz').split('')
   export default {
     name: 'QuestionSoal',
     props: {
@@ -191,6 +241,7 @@
     },
     data () {
       return {
+        abjad: abjad,
         data: {},
         user: this.$store.state.user.user,
         loadingProcess: false,
